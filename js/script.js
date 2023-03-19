cc371ef9b698707d750c702db86c0a6593626515
danielchavesfa <danielchavesfa@hotmail.com>
2023-03-19 12:29:07 -0300

    modal adicionado

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initAnimaScroll from './module/scroll-animacao.js';
 import scrollSuave from './module/scroll-suave.js';
 import initAccordion from './module/accordion.js';
 import initTabMenu from './module/tab-menu.js';
+import initModal from './module/modal.js';
 
 initAnimaScroll();
 scrollSuave();
 initAccordion();
 initTabMenu();
+initModal();

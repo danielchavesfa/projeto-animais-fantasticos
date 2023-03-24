562a4c0dbaa3babad52c744b6c2cc85a37af062b
danielchavesfa <danielchavesfa@hotmail.com>
2023-03-24 19:09:19 -0300

    pequeno ajuste na data

--- js/module/funcionamento.js
+++ js/module/funcionamento.js
@@ -1,21 +1,22 @@
 export default function initFuncionamento() {
-
-}
-
-const funcionamento = document.querySelector('[data-semana]');
-const diasSemana = funcionamento.dataset.semana.split(',').map(Number);
-const horarioSemana = funcionamento.dataset.horario.split(',').map(Number);
-
-const date = new Date();
-const diaAtual = date.getDay();
-const horaAtual = date.getHours();
-
-const diaAberto = diasSemana.indexOf(diaAtual) !== -1;
-const horarioAberto = horaAtual > horarioSemana[0] &&  horaAtual < horarioSemana[1];
-const verificacaoAberto = funcionamento.classList.contains('aberto');
-
-if(horarioAberto && diaAberto) {
-    verificacaoAberto ? null : funcionamento.classList.add('aberto');
-} else {
-    verificacaoAberto ? funcionamento.classList.remove('aberto') : null;
+    const funcionamento = document.querySelector('[data-semana]');
+    
+    if(funcionamento) {
+        const diasSemana = funcionamento.dataset.semana.split(',').map(Number);
+        const horarioSemana = funcionamento.dataset.horario.split(',').map(Number);
+        
+        const date = new Date();
+        const diaSemanaAtual = date.getDay();
+        const horaAtual = date.getHours();
+        
+        const diaAberto = diasSemana.indexOf(diaSemanaAtual) !== -1;
+        const horarioAberto = horaAtual > horarioSemana[0] &&  horaAtual < horarioSemana[1];
+        const verificacaoAberto = funcionamento.classList.contains('aberto');
+        
+        if(horarioAberto && diaAberto) {
+            verificacaoAberto ? null : funcionamento.classList.add('aberto');
+        } else {
+            verificacaoAberto ? funcionamento.classList.remove('aberto') : null;
+        }
+    }
 }

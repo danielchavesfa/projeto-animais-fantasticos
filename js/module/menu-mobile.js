b1d2370b4fc6747e1c2b8305f38014563cec7ae8
danielchavesfa <danielchavesfa@hotmail.com>
2023-04-03 14:41:40 -0300

    menu mobile refatorado

--- js/module/menu-mobile.js
+++ js/module/menu-mobile.js
@@ -1,22 +1,37 @@
 import outsideClick from './outsideclick.js';
 
-export default function initMenuMobile() {
-  const btnMenu = document.querySelector('[data-menu="button"]');
-  const menuList = document.querySelector('[data-menu="list"]');
-  const events = ['click', 'touchstart'];
+export default class MenuMobile {
+  constructor(btnMenu, menuList, events) {
+    this.btnMenu = document.querySelector(btnMenu);
+    this.menuList = document.querySelector(menuList);
 
-  function mostrarMenu() {
-    menuList.classList.add('active');
-    btnMenu.classList.add('active');
-    btnMenu.setAttribute('aria-expanded', 'true');
-    outsideClick(menuList, events, () => {
-      menuList.classList.remove('active');
-      btnMenu.classList.remove('active');
-      btnMenu.setAttribute('aria-expanded', 'false');
+    this.activeClass = 'active';
+
+    this.mostrarMenu = this.mostrarMenu.bind(this);
+
+    if (events === undefined) this.events = ['touchstart', 'click'];
+    else this.events = events;
+  }
+
+  mostrarMenu() {
+    this.menuList.classList.add(this.activeClass);
+    this.btnMenu.classList.add(this.activeClass);
+    this.btnMenu.setAttribute('aria-expanded', 'true');
+    outsideClick(this.menuList, this.events, () => {
+      this.menuList.classList.remove(this.activeClass);
+      this.btnMenu.classList.remove(this.activeClass);
+      this.btnMenu.setAttribute('aria-expanded', 'false');
     });
   }
 
-  if (btnMenu) {
-    events.forEach((userEvent) => btnMenu.addEventListener(userEvent, mostrarMenu));
+  addMenuMobileEvents() {
+    this.events.forEach((userEvent) => this.btnMenu.addEventListener(userEvent, this.mostrarMenu));
+  }
+
+  init() {
+    if (this.btnMenu && this.menuList) {
+      this.addMenuMobileEvents();
+    }
+    return this;
   }
 }
